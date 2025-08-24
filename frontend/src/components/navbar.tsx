4ac62cc5426f1e187548d429793fa5fa32dcdaad
CodeJr10 <aadityamalap666@gmail.com>
2025-08-24 20:37:43 +0530

    chore: hamburger button scraped

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -5,11 +5,9 @@ import download from "../assets/download_icon.svg";
 import { useState } from "react";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div>
-      <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+      <nav className="fixed w-full z-20 top-0 start-0 dark:border-gray-600">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a
             href="/"
@@ -19,35 +17,7 @@ const Navbar = () => {
               <img src={Logo} alt="CodeJr10 Logo" width={200} height={200} />
             </span>
           </a>
-          {/* Buttons */}
-          <div className="flex md:order-2 space-x-3 rtl:space-x-reverse">
-            {/* hamburger menu button */}
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              data-collapse-toggle="navbar-sticky"
-              type="button"
-              className=" inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 "
-              aria-controls="navbar-sticky"
-              aria-expanded={isOpen}
-            >
-              <span className="sr-only">Open Main Menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
-          </div>
+
           {/*  Nav Links */}
           <div
             className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
