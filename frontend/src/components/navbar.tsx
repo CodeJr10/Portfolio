5a6d5fb76ee180e53e7e4fc3a3c0dc8826cd228b
CodeJr10 <aadityamalap666@gmail.com>
2025-08-24 20:55:03 +0530

    chore: small css tweaks

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,8 +1,6 @@
 // import React from "react";
 
 import Logo from "../assets/logo.svg";
-import download from "../assets/download_icon.svg";
-import { useState } from "react";
 
 const Navbar = () => {
   return (
@@ -20,12 +18,13 @@ const Navbar = () => {
 
           {/*  Nav Links */}
           <div
-            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
-              isOpen ? "block" : "hidden"
-            }`}
+            className={`items-center justify-center w-full md:flex md:w-auto md:order-1`}
             id="navbar-sticky"
           >
-            <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 ">
+            <ul
+              className="flex fixed bottom-5 items-center justify-center p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-[#080930]
+            md:dark:bg-gray-900 dark:border-gray-700 "
+            >
               <li>
                 <a
                   href=""
@@ -53,15 +52,9 @@ const Navbar = () => {
               <li>
                 <a
                   href=""
-                  className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  className="py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Resume
-                  <img
-                    src={download}
-                    alt="Download Icon"
-                    width={15}
-                    height={15}
-                  />
                 </a>
               </li>
             </ul>
