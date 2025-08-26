bd22e887773d5c8a3fd52314c2d903484b6f8482
CodeJr10 <aadityamalap666@gmail.com>
2025-08-26 22:25:52 +0530

    chore: default styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,14 @@
 // import React from "react";
+import "./App.css";
+
+import Hero from "./components/hero";
 import Navbar from "./components/navbar";
+
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };
