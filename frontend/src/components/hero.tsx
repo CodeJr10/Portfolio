e7fa0d881c9fcde491c03de82c2cb7c50d2d868d
CodeJr10 <aadityamalap666@gmail.com>
2026-03-02 19:03:01 +0530

    Setup: Added hero component

---
+++ frontend/src/components/hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <section className='in-h-[calc(100vh-4rem)] md:min-h-[calc(100vh-5rem)]'></section>
+    </div>
+  )
+}
+
+export default Hero
