18d1b23b298ec1745d78a6ceeca3048b1fda995a
CodeJr10 <aadityamalap666@gmail.com>
2026-03-02 17:35:49 +0530

    Setup: Added Contact component

---
+++ frontend/src/components/contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+         <section className="min-h-screen flex items-center justify-center px-4">
+            <h1 className="text-3xl font-bold text-gray-800 dark:text-white">Contact Page</h1>
+         </section>
+      
+    </div>
+  )
+}
+
+export default Contact
