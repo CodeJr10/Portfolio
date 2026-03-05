ad820e1f4a4e7955ef3d23cdd6220897bfc62f3f
CodeJr10 <aadityamalap666@gmail.com>
2026-03-06 01:44:10 +0530

    Styling: Soda Rotation bottle

--- frontend/src/components/about.tsx
+++ frontend/src/components/about.tsx
@@ -1,14 +1,30 @@
-import React from 'react'
+import React from "react";
+import g2 from "../assets/g2.jpg";
+import g5 from "../assets/g5.jpg";
+
+interface CSSVar extends React.CSSProperties {
+  "--url"?: string;
+}
 
 const About = () => {
   return (
     <div>
-      <section className="min-h-screen flex items-center justify-center px-4">
+      <section className="min-h-screen flex items-center justify-center px-4"></section>
+      <div className="banner">
+        <div className="product">
+          <div
+            className="soda"
+            style={{ "--url": `url(${g2})` } as CSSVar}
+          ></div>
 
-      <p className="about-text">This is the About page.</p>
-      </section>
+          <div
+            className="soda soda-faded"
+            style={{ "--url": `url(${g5})` } as CSSVar}
+          ></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
