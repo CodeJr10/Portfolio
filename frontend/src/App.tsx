028fcc344975fc92a2cb17811a5aec2e97e3b898
CodeJr10 <aadityamalap666@gmail.com>
2026-03-09 01:01:59 +0530

    Chore: Added Images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,25 @@
 // import React from "react";
+
 import "./App.css";
+
 import About from "./components/about";
 import Contact from "./components/contact";
-import Projects from "./components/projects";
 import Hero from "./components/hero";
 import Navbar from "./components/navbar";
+import Projects from "./components/projects";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <main className="pt-16 md:pt-20">
-      <Hero />
+        <Hero />
       </main>
       <main className=" bg-white">
-
-      <About />
+        <About />
       </main>
+      <Projects />
       <Contact />
-      < Projects />
     </div>
   );
 };
